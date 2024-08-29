f64dc4d5546e71ff9232193c108e0b4e3c475fa1
Preetham <saipreetham1904@gmail.com>
2024-08-29 23:18:39 +0530

    initial commit

---
+++ src/components/chat/Chat.jsx
@@ -0,0 +1,128 @@
+import { useState } from "react";
+import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
+
+const Chat = () => {
+
+    const [open, setOpen] = useState(false);
+    const [text, setText] = useState("");
+
+    const handleEmoji = (e) => {
+        setText(prev => prev + e.emoji);
+        setOpen(false);
+    }
+
+    return (
+        <div className="chat">
+            <div className="top">
+                <div className="user">
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <span>Jane Doe</span>
+                        <p>Lorem ipsum dolor, sit amet.</p>
+                    </div>
+                </div>
+                <div className="icons">
+                    <img src="./phone.png" alt="" />
+                    <img src="./video.png" alt="" />
+                    <img src="./info.png" alt="" />
+                </div>
+            </div>
+            <div className="center">
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
+                            Aenean commodo ligula eget dolor. Aenean massa. Cum sociis 
+                            natoque penatibus et magnis dis parturient montes, nascetur 
+                            ridiculus mus. Donec quam felis, ultricies nec, pellentesque 
+                            eu, pretium quis, sem. Nulla consequat massa quis enim.
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message own">
+                    <div className="texts">
+                        <img src="./avatar.png" alt="" />
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
+                            Aenean commodo ligula eget dolor. Aenean massa. Cum sociis 
+                            natoque penatibus et magnis dis parturient montes, nascetur 
+                            ridiculus mus. Donec quam felis, ultricies nec, pellentesque 
+                            eu, pretium quis, sem. Nulla consequat massa quis enim.
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
+                            Aenean commodo ligula eget dolor. Aenean massa. Cum sociis 
+                            natoque penatibus et magnis dis parturient montes, nascetur 
+                            ridiculus mus. Donec quam felis, ultricies nec, pellentesque 
+                            eu, pretium quis, sem. Nulla consequat massa quis enim.
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message own">
+                    <div className="texts">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
+                            Aenean commodo ligula eget dolor. Aenean massa. Cum sociis 
+                            natoque penatibus et magnis dis parturient montes, nascetur 
+                            ridiculus mus. Donec quam felis, ultricies nec, pellentesque 
+                            eu, pretium quis, sem. Nulla consequat massa quis enim.
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
+                            Aenean commodo ligula eget dolor. Aenean massa. Cum sociis 
+                            natoque penatibus et magnis dis parturient montes, nascetur 
+                            ridiculus mus. Donec quam felis, ultricies nec, pellentesque 
+                            eu, pretium quis, sem. Nulla consequat massa quis enim.
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message own">
+                    <div className="texts">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
+                            Aenean commodo ligula eget dolor. Aenean massa. Cum sociis 
+                            natoque penatibus et magnis dis parturient montes, nascetur 
+                            ridiculus mus. Donec quam felis, ultricies nec, pellentesque 
+                            eu, pretium quis, sem. Nulla consequat massa quis enim.
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+            </div>
+            <div className="bottom">
+                <div className="icons">
+                    <img src="./img.png" alt="" />
+                    <img src="./camera.png" alt="" />
+                    <img src="./mic.png" alt="" />
+                </div>
+                <input type="text" value={text} placeholder="Type a message..." onChange={e => setText(e.target.value)} />
+                <div className="emoji">
+                    <img src="./emoji.png" alt="" onClick={() => setOpen(prev => !prev)} />
+                    <div className="picker">
+                        <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+                    </div>
+                </div>
+                <button className="sendButton">Send </button>
+            </div>
+        </div>
+    )
+}
+
+export default Chat;
